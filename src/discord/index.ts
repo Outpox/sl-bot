a64347d768fbd525f048446441ab88cb52fdc09c
Hugo TORZUOLI <hugo.torzuoli@versusmind.eu>
2019-07-19 23:12:41 +0200

    Beautify RichEmbed - Custom Emoji - Reduce platform numbers - Add song thumbnail - Color of Rich Embed

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -5,17 +5,17 @@ import { platforms } from '../songlink'
 export function richEmbedfromSonglinkResponse(author: User, response: ParsedSonglinkResponse): RichEmbed {
   const config: RichEmbedOptions = {
     title: 'Songlink',
-    description: `${author.username} has shared **${response.title}** by **${response.artist}**`,
-    image: { url: response.cover },
-    thumbnail: { url: 'https://assets.outpox.fr/songlink.png' },
+    description: `<@!${author.id}> has shared **${response.title}** by **${response.artist}**`,
+    color: 0X00c4b0,
+    thumbnail: { url: response.cover },
     fields: platforms
       .filter(platform => {
-        return response.linksByPlatform[platform] !== undefined
+        return response.linksByPlatform[platform.name] !== undefined
       })
       .map(platform => {
         return {
-          name: platform,
-          value: `[${platform}](${response.linksByPlatform[platform].url})`,
+          name: platform.name.charAt(0).toUpperCase() + platform.name.slice(1),
+          value: `[<:${platform.name}:${platform.emoji}>](${response.linksByPlatform[platform.name].url})`,
           inline: true,
         }
       }),
